7ec978c48fc0ddab1e42929dd670cfe157eee67e
Pitt Wu <kgb00128@gmail.com>
2020-02-15 13:17:49 +0800

    增加 todoList 功能

--- js/app.js
+++ js/app.js
@@ -5,3 +5,30 @@ allData.on('value', (snapshot) => {
   path.textContent = JSON.stringify(snapshot.val(), null, 2)
 })
 // --分隔--
+// Dom 操作
+let txt = document.querySelector('.txt')
+let btn = document.querySelector('.btn')
+let list = document.querySelector('.list')
+let todoList = firebase.database().ref('todoList')
+
+// send => save to database
+btn.addEventListener('click', () => {
+  todoList.push({contentList: txt.value}) 
+})
+// watch database
+todoList.on('value', (snapshot) => {
+  let str = ''
+  let data = snapshot.val()
+  for (let node in data) {
+    // 插入 database 每筆資料的 key 到標籤上，data-key 是固定寫法
+    str += '<li data-key="'+ node +'">' + data[node].contentList + '</li>'
+  }
+  list.innerHTML = str
+})
+// remove
+list.addEventListener('click', (val) => {
+  if (val.target.nodeName === 'LI') {
+    let key = val.target.dataset.key
+    todoList.child(key).remove()
+  }
+})
