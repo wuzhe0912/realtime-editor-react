5b6fee56f68b2507f4a1dd76f5988755d16c8ec8
Pitt Wu <pitt.wu@hlwtech.com.tw>
2021-08-11 17:37:21 +0800

    feat: create http server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+
+const PORT = 5000;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const io = require('socket.io')(server);
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
