ad1f55bdf7c4051ceb5f4c9439d87c07c0f6b60e
Pitt Wu <kgb00128@gmail.com>
2022-05-26 11:13:01 +0800

    fix : home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import codeSyncLogo from "assets/code-sync.png";
+
 function Home() {
   return (
-    <div className="home-wrapper">
-      <div className="form-wrapper">
-        <img src="assets/code-sync.png" alt="code-sync-logo" />
+    <main className="home-wrapper">
+      <section className="form-wrapper">
+        <img src={codeSyncLogo} alt="code-sync-logo" />
         <h4 className="invite-label">Paste invitation ROOM ID</h4>
         <div className="input-container">
           <input type="text" className="input-box" placeholder="ROOM ID" />
@@ -12,11 +14,25 @@ function Home() {
           <button className="btn btn-join">Join</button>
           <span className="create-info">
             If you don't have an invite then create new &nbsp;
-            <a href=""></a>
+            <a href="https://www.google.com/" className="create-room-btn">
+              new room
+            </a>
           </span>
         </div>
-      </div>
-    </div>
+      </section>
+      <footer>
+        <h4>
+          Built with React by&nbsp;
+          <a
+            href="https://github.com/wuzhe0912"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Pitt Wu
+          </a>
+        </h4>
+      </footer>
+    </main>
   );
 }
 
