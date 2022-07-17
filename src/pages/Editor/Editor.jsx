e63337dbe743e8392fbb4653337b3180d3e25c13
Pitt Wu <kgb00128@gmail.com>
2022-07-17 23:03:53 +0800

    feat: setting socket

--- src/pages/Editor/Editor.jsx
+++ src/pages/Editor/Editor.jsx
@@ -1,8 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import toast from 'react-hot-toast';
-import { useNavigate, useParams } from 'react-router-dom';
 import Client from 'components/Client/Client';
 import EditorContent from 'components/EditorContent/EditorContent';
+import { initSocket } from 'server/socket';
+import ACTIONS from 'constant/Actions';
+import {
+  useNavigate,
+  useParams,
+  useLocation,
+  Navigate,
+} from 'react-router-dom';
 // styles
 import commonStyles from 'common/common.module.css';
 import styles from './Editor.module.css';
@@ -10,11 +17,40 @@ import CodeSyncLogo from 'assets/code-sync.png';
 
 function Editor() {
   const [clients, setClients] = useState([]);
-  const reactNavigator = useNavigate();
+  const socketRef = useRef(null);
+  const location = useLocation();
   const { roomId } = useParams();
+  const reactNavigator = useNavigate();
 
   useEffect(() => {
-    console.log(1, roomId);
+    const init = async () => {
+      socketRef.current = await initSocket();
+      // check connection status, if disconnected then redirect to home and show error toast
+      socketRef.current.on('connect_error', (error) => {
+        return handleErrors(error);
+      });
+      socketRef.current.on('connect_failed', (error) => {
+        return handleErrors(error);
+      });
+
+      function handleErrors(error) {
+        toast.error('Socket connection failed, try again later.');
+        console.log('socket error', error);
+        reactNavigator('/');
+      }
+
+      console.log(1, location);
+      // connect is successful, join room
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.userName,
+      });
+
+      // socketRef.current.on(ACTIONS.JOIN, ({ clients }) => {
+      //   console.log('clients', clients);
+      // });
+    };
+    init();
   }, []);
 
   const copyRoomId = async () => {
@@ -29,6 +65,11 @@ function Editor() {
 
   function leaveRoom() {
     reactNavigator('/');
+  }
+
+  // block illegal navigation and redirect to home
+  if (!location.state) {
+    return <Navigate to='/' />;
   }
 
   return (
@@ -65,7 +106,7 @@ function Editor() {
         </div>
       </aside>
       <section className='editor-content'>
-        <EditorContent roomId={roomId} />
+        <EditorContent roomId={roomId} socketRef={socketRef} />
       </section>
     </div>
   );
--- src/server/server.js
+++ src/server/server.js
@@ -5,15 +5,40 @@ const { Server } = require('socket.io');
 const ACTIONS = require('../constant/Actions.js');
 
 const httpServer = http.createServer(app);
-
 const socketIo = new Server(httpServer);
+
+const userSocketMap = {};
+function getAllConnectedClients(roomId) {
+  return Array.from(socketIo.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    },
+  );
+}
 
 // websocket api
 socketIo.on('connection', (socket) => {
-  console.log('socket', socket.id);
+  console.log(1, 'server', socket.id);
+
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    console.log(2, userSocketMap[socket.id]);
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    clients.forEach(({ socketId }) => {
+      socketIo.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
